173ce7941c691777a63ca31598a9ce175e5d5516
Salvador Guzman <guzmansalv@gmail.com>
2025-12-27 17:52:50 -0600

    toml format

--- toml.format.js
+++ toml.format.js
@@ -1,3 +1,6 @@
 function tomlFormat(cat) {
-    return globalThis.__podcastFeeds.map(a => `[[feeds]]\nid = "podcasts-${cat}-${a.id}"\nurl = "${a.feedUrl}"\nbase_poll_seconds = 1000\n`);
+    const string = globalThis.__podcastFeeds.map(a => `[[feeds]]\nid = "podcasts-${cat}-${a.id}"\nurl = "${a.feedUrl}"\nbase_poll_seconds = 1000\n`);
+
+    console.log(string.join('\n'))
 }
+
